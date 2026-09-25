fb3531a260f7810b3858d48712198a48a299d084
parthrajrana <ranaparthraj23@gmail.com>
2026-09-25 09:46:33 +0530

    Update allowed origins for CORS middleware

--- Backend/main.py
+++ Backend/main.py
@@ -55,7 +55,7 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://cardiovascular-disease-prediction-lac.vercel.app"
+    "https://cardio-vascular-prediction.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
